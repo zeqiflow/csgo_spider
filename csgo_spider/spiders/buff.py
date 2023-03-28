1d88428d660f8825ca787b94dd0cf8cd0ca20e81
zeqiflow <zeqig@outlook.com>
2023-03-28 08:57:37 +0800

    buff spider test

--- csgo_spider/spiders/buff.py
+++ csgo_spider/spiders/buff.py
@@ -1,10 +1,25 @@
-import scrapy
+import sys
+sys.path.append('/Users/apple/Desktop/developer/csgo_spider/csgo_spider')
 
+import scrapy
+from items import CsgoItem
+from icecream import ic
 
 class BuffSpider(scrapy.Spider):
     name = "buff"
     allowed_domains = ["buff.163.com"]
-    start_urls = ["http://buff.163.com/"]
+    start_urls = ["https://buff.163.com/market/csgo#tab=selling&page_num=1&category=weapon_knife_skeleton"]
 
     def parse(self, response):
-        pass
+        items = []
+    
+        for each in response.xpath("//*[@id=\"j_list_card\"]"):
+            item = CsgoItem()
+            name = each.extract()
+
+            # price = each.xpath("./td[@class=\"text-no-wrap\"]/h3/a/text()").extract()
+            item['name'] = name[0].strip()
+            # item['price'] = price[0].strip()
+
+            items.append(item)
+        return items
